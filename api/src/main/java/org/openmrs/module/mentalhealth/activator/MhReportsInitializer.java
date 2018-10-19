0b39fd92f32303ec21d8bf3f0462c74f6c3364ab
Ken Long <long27km@uw.edu>
2018-10-19 10:51:55 -0700

    implements cascade analysis report only for schizophrenia

--- api/src/main/java/org/openmrs/module/mentalhealth/activator/MhReportsInitializer.java
+++ api/src/main/java/org/openmrs/module/mentalhealth/activator/MhReportsInitializer.java
@@ -5,12 +5,16 @@ import org.apache.commons.logging.LogFactory;
 import org.openmrs.api.AdministrationService;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.mentalhealth.MhReportManager;
+import org.openmrs.module.mentalhealth.reports.CascadeAnalysisReport;
+import org.openmrs.module.mentalhealth.reports.SampleReport;
 import org.openmrs.module.reporting.ReportingConstants;
 import org.openmrs.module.reporting.report.manager.ReportManagerUtil;
 import org.openmrs.module.reporting.report.util.ReportUtil;
 
 public class MhReportsInitializer implements MhInitializer {
 
+	static final String SAMPLE_XLS_TEMPLATE_UUID = "83684b4c-d062-11e8-89d2-938f982d9cb7";
+	
     protected static final Log log = LogFactory.getLog(MhReportsInitializer.class);
 
     @Override
@@ -36,19 +40,33 @@ public class MhReportsInitializer implements MhInitializer {
         // the purpose of this snipet is to allow rapid development other than
         // going to change the report version all the time for change
         log.warn("Removing old reports");
-        // getting id of the loaded report designs
-        String report_resource_sample_id = "select id from reporting_report_design where uuid='83684b4c-d062-11e8-89d2-938f982d9cb7'";
-        // deleting the resource already loaded
-        as.executeSQL("delete from reporting_report_design_resource where report_design_id =("
+        
+        String []oldReportDesignUUIDs = 	{
+        								SampleReport.XLS_TEMPLATE_UUID,
+        								CascadeAnalysisReport.XLS_TEMPLATE_UUID//""
+        							};
+        
+        for(int i=0; i<oldReportDesignUUIDs.length; i++) {
+        	// getting id of the loaded report designs
+        	String report_resource_sample_id = "select id from reporting_report_design where uuid='"+oldReportDesignUUIDs[i]+"'";
+        	// deleting the resource already loaded
+        	as.executeSQL("delete from reporting_report_design_resource where report_design_id =("
                 + report_resource_sample_id + ");", false);
-        // deleting the actual designs now
-        as.executeSQL("delete from reporting_report_design where uuid='83684b4c-d062-11e8-89d2-938f982d9cb7';", false);
-
+        	// deleting the actual designs now
+        	as.executeSQL("delete from reporting_report_design where uuid='"+oldReportDesignUUIDs[i]+"';", false);
+        }
         // deleting all report requests and managers
         as.executeSQL("delete from reporting_report_request;", false);
         as.executeSQL("delete from global_property WHERE property LIKE 'reporting.reportManager%';", false);
 
-        // deleting the actual report definitions from the db
-        as.executeSQL("delete from serialized_object WHERE uuid = '94110b96-d062-11e8-9f06-a3af958caf7f';", false);
+        String []oldReportDefinitionUUIDs = {
+        										SampleReport.REPORT_DEFINITION_UUID,
+        										CascadeAnalysisReport.REPORT_DEFINITION_UUID,
+        };
+        
+        for(int i=0; i<oldReportDefinitionUUIDs.length; i++) {
+        	// deleting the actual report definitions from the db
+        	as.executeSQL("delete from serialized_object WHERE uuid = '"+oldReportDefinitionUUIDs[i]+"';", false);
+        }
     }
 }
