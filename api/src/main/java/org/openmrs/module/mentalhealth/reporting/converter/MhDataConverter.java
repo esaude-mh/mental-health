c707e732b9413a598e146db768efc819c4bc9c27
ningosi <nicotwendelee@gmail.com>
2019-04-12 12:52:57 +0300

    Add in more columns

---
+++ api/src/main/java/org/openmrs/module/mentalhealth/reporting/converter/MhDataConverter.java
@@ -0,0 +1,39 @@
+package org.openmrs.module.mentalhealth.reporting.converter;
+
+import org.openmrs.Encounter;
+import org.openmrs.module.reporting.data.converter.DataConverter;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MhDataConverter implements DataConverter {
+    @Override
+    public Object convert(Object obj) {
+
+        if (obj == null) {
+            return "";
+        }
+
+        Encounter value = (Encounter) obj;
+        if(value.getEncounterDatetime() != null) {
+            return formatDate(value.getEncounterDatetime());
+        }
+        return null;
+    }
+
+    @Override
+    public Class<?> getInputDataType() {
+        return Encounter.class;
+    }
+
+    @Override
+    public Class<?> getDataType() {
+        return String.class;
+    }
+
+    private String formatDate(Date date) {
+        DateFormat dateFormatter = new SimpleDateFormat("dd-MMM-yyyy");
+        return date == null?"":dateFormatter.format(date);
+    }
+}
--- api/src/main/java/org/openmrs/module/mentalhealth/reports/DateExportReport.java
+++ api/src/main/java/org/openmrs/module/mentalhealth/reports/DateExportReport.java
@@ -1,17 +1,22 @@
 package org.openmrs.module.mentalhealth.reports;
 
+import org.openmrs.EncounterType;
 import org.openmrs.PatientIdentifierType;
 import org.openmrs.module.mentalhealth.MhDataExportManager;
+import org.openmrs.module.mentalhealth.metadata.MentalHealthEncounterTypes;
 import org.openmrs.module.mentalhealth.metadata.MentalHealthPatientIdentifierTypes;
+import org.openmrs.module.mentalhealth.reporting.converter.MhDataConverter;
+import org.openmrs.module.mentalhealth.utils.DataFactory;
 import org.openmrs.module.metadatadeploy.MetadataUtils;
+import org.openmrs.module.reporting.common.SortCriteria;
+import org.openmrs.module.reporting.common.TimeQualifier;
 import org.openmrs.module.reporting.data.DataDefinition;
 import org.openmrs.module.reporting.data.converter.DataConverter;
 import org.openmrs.module.reporting.data.converter.ObjectFormatter;
 import org.openmrs.module.reporting.data.patient.definition.ConvertedPatientDataDefinition;
+import org.openmrs.module.reporting.data.patient.definition.EncountersForPatientDataDefinition;
 import org.openmrs.module.reporting.data.patient.definition.PatientIdentifierDataDefinition;
 import org.openmrs.module.reporting.data.patient.library.BuiltInPatientDataLibrary;
-import org.openmrs.module.reporting.data.person.definition.AgeDataDefinition;
-import org.openmrs.module.reporting.data.person.definition.GenderDataDefinition;
 import org.openmrs.module.reporting.data.person.definition.PreferredNameDataDefinition;
 import org.openmrs.module.reporting.dataset.definition.DataSetDefinition;
 import org.openmrs.module.reporting.dataset.definition.PatientDataSetDefinition;
@@ -21,6 +26,7 @@ import org.openmrs.module.reporting.report.definition.ReportDefinition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.Properties;
 
 @Component
@@ -28,6 +34,9 @@ public class DateExportReport extends MhDataExportManager {
 
     @Autowired
     private BuiltInPatientDataLibrary builtInPatientData;
+
+    @Autowired
+    private DataFactory factory;
 
     public static final String REPORT_NAME = "Data Export Report";
     @Override
@@ -75,12 +84,13 @@ public class DateExportReport extends MhDataExportManager {
 
     @Override
     public String getVersion() {
-        return "0.1";
+        return "0.1.1";
     }
 
     private DataSetDefinition dataSetDefinition() {
         PatientDataSetDefinition dsd = new PatientDataSetDefinition();
         dsd.setName("export");
+        dsd.addSortCriteria("consult1", SortCriteria.SortDirection.ASC);
 
         //identifier
         PatientIdentifierType mhNumber = MetadataUtils.existing(PatientIdentifierType.class, MentalHealthPatientIdentifierTypes.MH_NID.uuid());
@@ -89,13 +99,20 @@ public class DateExportReport extends MhDataExportManager {
 
         dsd.addColumn("NID", identifierDef, "");
         dsd.addColumn("Nome", new PreferredNameDataDefinition(), (String) null);
-        dsd.addColumn("province", new PreferredNameDataDefinition(), (String) null);
-        dsd.addColumn("district", new PreferredNameDataDefinition(), (String) null);
-        dsd.addColumn("consult1", new PreferredNameDataDefinition(), (String) null);
+        addColumn(dsd,"province",factory.getPreferredAddress("stateProvince"));
+        addColumn(dsd,"district",factory.getPreferredAddress("countyDistrict"));
+        dsd.addColumn("consult1", firstDateOfConsultations(), "", new MhDataConverter());
         addColumn(dsd, "Gender", builtInPatientData.getGender());
         addColumn(dsd, "age", builtInPatientData.getAgeAtEnd());
 
 
+        return dsd;
+    }
+
+    private DataDefinition firstDateOfConsultations(){
+        EncountersForPatientDataDefinition dsd = new EncountersForPatientDataDefinition();
+        dsd.setWhich(TimeQualifier.FIRST);
+        dsd.setTypes(Arrays.asList(MetadataUtils.existing(EncounterType.class, MentalHealthEncounterTypes.INITIAL_ENCOUNTER_TYPE.uuid())));
         return dsd;
     }
 }
