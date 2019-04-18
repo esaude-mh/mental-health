e29eeadc52f8ff1004d37f6766b07cc1ca3ced30
ningosi <nicotwendelee@gmail.com>
2019-04-18 20:01:40 +0300

    Map concepts for the initial form

--- api/src/main/java/org/openmrs/module/mentalhealth/utils/MhConstants.java
+++ api/src/main/java/org/openmrs/module/mentalhealth/utils/MhConstants.java
@@ -43,26 +43,26 @@ public class MhConstants {
     public static final String ALCOHOLISM = "6360c0bb-908a-11e6-a98e-000c29db4475";
     public static final String OTHER_SUBSTANCE = "8a3d44c6-d428-406f-9b46-d6229756548b";
     public static final String OTHER_SUBSTANCE_YES = "5a9ad37b-f330-44de-afad-6330a9eb4673";
-    public static final String OTHER_CHRONIC_DISEASES = "";
-    public static final String OTHER_CHRONIC_DISEASE_SPECIFY = "";
-    public static final String TB_TEST = "";
-    public static final String TB_TEST_DIAGNOSED = "";
-    public static final String HIV_TEST = "";
-    public static final String HIV_DIAGNOSED_DATE = "";
-    public static final String HIV_START_DATE = "";
-    public static final String ALCOHOL_USE = "";
-    public static final String ALCOHOL_USE_DESC = "";
-    public static final String USE_OF_OTHER_DRUGS = "";
-    public static final String USE_OF_OTHER_DRUGS_DESC = "";
-    public static final String USE_TRADITIOANL_MEDICINE = "";
-    public static final String USE_TRADITIONAL_MEDICINE_DESC = "";
-    public static final String APPEARENCE_POSTURE = "";
-    public static final String SYNTHESIS_FUNCTIONS = "";
-    public static final String SENSIORY_PERCEPTION = "";
-    public static final String THOUGHTS_JUDGMENT_CRITICAL = "";
-    public static final String HUMOR = "";
-    public static final String CONDUCT = "";
-    public static final String RELATIONSHIP_FUNCTIONS = "";
+    public static final String OTHER_CHRONIC_DISEASES = "8b928244-ef8f-47ea-a78a-07bce56ca7bc";
+    public static final String OTHER_CHRONIC_DISEASE_SPECIFY = "aa8505c4-7037-4478-af8c-34b2c6dd760c";
+    public static final String TB_TEST = "efd69778-eac1-4132-88be-1222af97e6b8";
+    public static final String TB_TEST_DIAGNOSED = "743d7058-ee4f-49a5-8266-80b85b416ecb";
+    public static final String HIV_TEST = "e1dd198c-1d5f-11e0-b929-000c29ad1d07";
+    public static final String HIV_DIAGNOSED_DATE = "54b65bbd-a0b0-4dfa-a3b1-de14d41bc314";
+    public static final String HIV_START_DATE = "aa75aafb-fb9a-4ab6-90fd-8354898df2a1";
+    public static final String ALCOHOL_USE = "e1dcabf0-1d5f-11e0-b929-000c29ad1d07";
+    public static final String ALCOHOL_USE_DESC = "8c1cb1c7-67c7-4923-8f10-0f2c03a2ac63";
+    public static final String USE_OF_OTHER_DRUGS = "2d2c6fdd-0065-4ec6-9f87-0cbddb4e5ac1";
+    public static final String USE_OF_OTHER_DRUGS_DESC = "a2329316-bce3-4d81-9928-97f093b32549";
+    public static final String USE_TRADITIOANL_MEDICINE = "b222e3ba-b9fc-4f0d-b9fd-23cc8e65ebf8";
+    public static final String USE_TRADITIONAL_MEDICINE_DESC = "83baba80-73fd-4621-8279-83a28396046f";
+    public static final String APPEARENCE_POSTURE = "f1c78c38-f015-4675-852b-39785fe3319d";
+    public static final String SYNTHESIS_FUNCTIONS = "23bace48-763d-4996-9563-abba1e37f236";
+    public static final String SENSIORY_PERCEPTION = "935ef714-7f50-47d7-a31e-f21d9f401695";
+    public static final String THOUGHTS_JUDGMENT_CRITICAL = "5c72c0e6-94a2-4ab5-8bf5-ec5bd2331aff";
+    public static final String HUMOR = "2be2c4fa-f168-4ff2-b4e1-05b8979306d0";
+    public static final String CONDUCT = "6722a5d1-dc1c-44e9-8c90-4256974b7849";
+    public static final String RELATIONSHIP_FUNCTIONS = "f5bf98f5-34bb-4e28-b7ec-2312654e5aa1";
 
 
 
